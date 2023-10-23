9da30adc27c7f87ddfc6fa94df7eada4c71a461a
Hussain <hedayati1488@gmail.com>
2023-10-23 11:53:57 +0330

    new changes

--- resources/views/back/cooperationsales/Income.blade.php
+++ resources/views/back/cooperationsales/Income.blade.php
@@ -1,7 +1,6 @@
 @extends('back.layouts.master')
 
 @section('content')
-
     <div class="app-content content">
         <div class="content-overlay"></div>
         <div class="header-navbar-shadow"></div>
@@ -26,83 +25,78 @@
 
             </div>
             <div class="content-body">
-            <section class="card">
+                <section class="card">
                     <div class="card-header">
-                        <h4 class="card-title">اسم کاربر</h4>
+                        <h4 class="card-title">فروش های پرداخت شده کاربر</h4>
                     </div>
                     <div class="card-content">
                         <div class="container mt-3">
 
-                            <!-- Nav tabs -->
-                            <ul class="nav nav-tabs">
-                                <li class="nav-item">
-                                    <a class="nav-link active" style="font-size: 10px" data-toggle="tab" href="#home">در انتظار تأیید</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" style="font-size: 10px" data-toggle="tab" href="#menu1">اقساط تأیید نشده</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" style="font-size: 10px" data-toggle="tab" href="#menu3">اقساط تأیید شده</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" style="font-size: 10px" data-toggle="tab" href="#menu4">خدمات اینترنتی</a>
-                                </li>
-                            </ul>
 
                             <!-- Tab panes -->
-                            <div class="tab-content">
-                                <div id="home" class="container tab-pane active"><br>
-                                    <div class="row">
+                            <div id="home" class="container tab-pane active"><br>
+                                <div class="row">
 
-                                        <div class="col-md-6 col-12">
-                                            <div class="form-group d-flex align-items-center">
-                                                <label for="first_name" class="mr-2">
-                                                    مقدار اعتبار خرید اقساطی
-                                                </label>
-                                                <div class="d-flex">
-                                                    <input type="text" placeholder="100,000"
-                                                        class="form-control moneyInput" id="first_name" name="first_name">
-                                                </div>
-
+                                    <div class="col-md-6 col-12">
+                                        <div class="form-group d-flex align-items-center">
+                                            <label for="first_name" class="mr-2">
+                                                مقدار اعتبار خرید اقساطی
+                                            </label>
+                                            <div class="d-flex">
+                                                <input type="text" placeholder="100,000" class="form-control moneyInput"
+                                                    id="first_name" name="first_name">
                                             </div>
-                                        </div>
-                                        <div class="col-md-6 col-12">
 
                                         </div>
                                     </div>
-                                    <div class="border rounded p-2">
-                                        <div class="row">
-                                            <h5>
-                                                اسم کاربر
-                                            </h5>
-                                        </div>
+                                    <div class="col-md-6 col-12">
 
-
-                                        <div class="row">
-                                            مبلغ کل فروش:‌۱۰۰۰۰
-                                        </div>
-                                        <div class="row">
-                                            ۱۵ عدد قسط به سر رسیده ۲۵ هر ماه به مبلغ قسط ۱۰۰،۰۰۰ ریال
-                                        </div>
-
-                                        <div class="row mt-2">
-                                            مقدار پیش پرداخت ۳۰۰،۰۰۰ ریال
-                                        </div>
-                                    </div>
-
-                                    <a href="" class="btn btn-danger my-1">انصراف از فروش</a>
-                                </div>
-                                    <div id="menu1" class="container tab-pane fade"><br>
-                                        <h3>Menu 1</h3>
-                                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                            aliquip ex ea commodo consequat.</p>
-                                    </div>
-                                    <div id="menu2" class="container tab-pane fade"><br>
-                                        <h3>Menu 2</h3>
-                                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-                                            doloremque laudantium, totam rem aperiam.</p>
                                     </div>
                                 </div>
+                                <div class="border rounded p-2">
+                                    <div class="row text-center" style="flex-direction: column;">
+                                        <h5>
+                                            آقای محسن حسین زاده
+                                        </h5>
+                                    </div>
+
+
+                                    <div class="row">
+                                        مبلغ کل فروش: ۱۰۰،۰۰۰ ریال
+                                    </div>
+                                    <div class="row">
+                                        ۱۵ عدد قسط به سر رسیده ۲۵ هر ماه به مبلغ قسط ۱۰۰،۰۰۰ ریال
+                                    </div>
+
+                                    <div class="row mt-2">
+                                        تاریخ فروش:‌۱۴۰۲/۲/۱۳
+                                    </div>
+                                    <div class="row my-1 px-1 text-center">
+                                        ۱۴ روز بعد از فروش گزینه درخواست تسویه فعال میشود و مبلغ بعد از کسر کارمزد به کیف
+                                        پول شما انتقال پیدا میکند.
+                                    </div>
+                                </div>
+                                <div class="row mx-4" style="flex-direction: column;">
+                                    <a href="" class="btn btn-danger my-1">درخواست تسویه </a>
+                                </div>
+                                <div class="row mt-4">
+                                    <p class="text-center">
+                                        در صورتی که تأمین کننده کالا فردی غیر از شمامیباشد فاکتور فروش از نام فروشنده به
+                                        خریدار تهیه و در قسمت عکس فاکتور آپلود نمایید.
+                                    </p>
+                                    <div class="">
+                                        <input type="file" accept=".pdf, .doc" accept-language="fa">
+                                        <label for="">افزودن عکس فاکتور فروش</label>
+                                        {{-- <br>
+                                        <input type="file" class="custom-file-input" id="customFile">
+                                        <label class="custom-file-label" for="customFile">انتخاب فایل</label> --}}
+                                    </div>
+                                    <div class="">
+                                        <input type="text">
+                                        <label for="">ثبت شماره شبا</label>
+                                    </div>
+                                </div>
+
                             </div>
 
                         </div>
@@ -111,5 +105,6 @@
             </div>
         </div>
     </div>
-
 @endsection
+
+
