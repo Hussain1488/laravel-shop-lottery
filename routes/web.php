d01906e9c01f1fc47d06de5d54ede28ecc942d5e
hussain1488 <hedayati1488@gmail.com>
2023-10-28 12:49:03 +0330

    new routes for installmentsreports created.

--- routes/web.php
+++ routes/web.php
@@ -118,6 +118,8 @@ Route::group(['as' => 'admin.', 'prefix' => 'admin/' . admin_route_prefix(), 'mi
 
     // ------------------ installment Reports
     Route::get('installmentreports/index', [InstallmentReportsController::class, 'index'])->name('installmentreports.index');
+    Route::get('installmentreports/filter', [InstallmentReportsController::class, 'filter'])->name('installments.filter');
+    Route::get('installmentreports/filter1', [InstallmentReportsController::class, 'filter1'])->name('installments.filter1');
 
 
     // ------------------ users
