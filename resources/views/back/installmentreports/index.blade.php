fd21909ec5fc7a88b405e4fb791cdef88ad42eae
hussain1488 <hedayati1488@gmail.com>
2023-10-28 12:48:38 +0330

    tabs, filter, and forms created.

--- resources/views/back/installmentreports/index.blade.php
+++ resources/views/back/installmentreports/index.blade.php
@@ -35,22 +35,29 @@
                             </h4>
                         @endisset --}}
                     </div>
+
+
+
+
                     <div class="card-content">
                         <div class="container mt-3">
 
                             <!-- Nav tabs -->
                             <ul class="nav nav-tabs">
                                 <li class="nav-item">
-                                    <a class="nav-link active " style="font-size: 10px" data-toggle="tab" href="#home">در
+                                    <a class="nav-link {{ $payment_stat == 'wait' ? 'active' : '' }} "
+                                        style="font-size: 10px" data-toggle="tab" href="#home">در
                                         انتظار تأیید</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" style="font-size: 10px" data-toggle="tab" href="#menu1">اقساط
+                                    <a class="nav-link {{ $payment_stat == 'not_paid' ? 'active' : '' }}"
+                                        style="font-size: 10px" data-toggle="tab" href="#menu1">اقساط
                                         پرداخت
                                         نشده</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" style="font-size: 10px" data-toggle="tab" href="#menu2">اقساط
+                                    <a class="nav-link {{ $payment_stat == 'paid' ? 'active' : '' }}"
+                                        style="font-size: 10px" data-toggle="tab" href="#menu2">اقساط
                                         پرداخت
                                         شده</a>
                                 </li>
@@ -58,62 +65,161 @@
 
                             <!-- Tab panes -->
                             <div class="tab-content">
-                                <div id="home" class="container tab-pane active"><br>
-                                    <div class="row">
+                                <div id="home"
+                                    class="container tab-pane {{ $payment_stat == 'wait' ? 'active' : 'fade' }}"><br>
 
-                                        <div class="col-md-6 col-12">
+                                    <form action="{{ route('admin.installments.filter') }}" method="get">
+                                        @csrf
+                                        <div class="row ">
 
+                                            <div class="col-md-6 col-12 d-flex justify-content-around">
 
+                                                <h4>
+                                                    فیلتر بر اساس شماره تلفن
+                                                </h4>
 
-                                        </div>
-                                        <div class="col-md-6 col-12">
-
-                                        </div>
-                                    </div>
-                                    {{-- @empty(false)
-                                        <section id="main-card" class="card">
-                                            <div class="card-header m-3 ">
-                                                <h3 class="text-danger">لیست فروشی برای نمایش به شما وجود ندارد</h3>
+                                                <div class="d-flex">
+                                                    <input type="text" name="filter" class="form-control w-auto mr-1"
+                                                        placeholder="شماره تلفن را برای فیلتر وارد کنید">
+                                                    <input type="submit" class="btn btn-info" value="فیلتر">
+                                                </div>
                                             </div>
-                                        </section>
-                                    @else --}}
-                                    {{-- @foreach ($installmentsm as $key) --}}
-                                    {{-- @if ($key->status == 0) --}}
-                                    <div class="border rounded p-2 my-1">
-                                        <div class="row d-flex justify-content-center">
-                                            <h5>قسط فروشگاه:
-                                                احمدی
-                                            </h5>
-                                        </div>
-
-
-                                        <div class="row">
-                                            مبلغ کل فروش:۱۰۰۰۰
-                                        </div>
-                                        <div class="row">
-                                            ۲۵ عدد قسط به سر رسیده
-                                            ۶ هر ماه به مبلغ قسط
-                                            ۱۰۰۰۰ ریال
-                                        </div>
-
-                                        <div class="row mt-2">
-                                            <div class="col">
-                                                مقدار پیش پرداخت ۱۰۰۰۰ ریال
+                                            <div class="col-md-6 col-12">
                                             </div>
                                         </div>
+                                    </form>
+
+                                    <div class="row mt-1 ml-2">
+                                        <h3>
+                                            لیست اقساطی که در انتظار نشده:
+                                        </h3>
                                     </div>
+
+                                    @if (empty($installments))
+                                        <div class="row mt-3 ml-2">
+                                            <h4>
+                                                هیچ قسطی برای نمایش وجود ندارد!
+                                            </h4>
+                                        </div>
+                                    @else
+                                        @foreach ($installments as $key)
+                                            @if ($key->status == 0)
+                                                {{-- <div class="row mt-3 ml-2">
+                                                    <h4>
+                                                        اقساط فروشگاه: {{ $key->seller->first_name }}
+                                                    </h4>
+                                                </div> --}}
+
+
+
+                                                <div class="border rounded p-2 my-1">
+                                                    <div class="row d-flex justify-content-center">
+                                                        <h5>قسط آقای: {{ $key->user->username }}
+                                                        </h5>
+                                                    </div>
+
+
+                                                    <div class="row">
+                                                        مبلغ کل فروش:{{ $key->Creditamount }}
+                                                    </div>
+                                                    <div class="row">
+                                                        {{ $key->numberofinstallments }} عدد قسط به سر رسیده
+                                                        ۶ هر ماه به مبلغ قسط
+                                                        {{ $key->amounteachinstallment }} ریال
+                                                    </div>
+
+                                                    <div class="row mt-2">
+                                                        <div class="col">
+                                                            مقدار پیش پرداخت {{ $key->prepaidamount }} ریال
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            @endif
+                                        @endforeach
+                                    @endif
                                     {{-- @endif --}}
                                     {{-- @endforeach --}}
                                     {{-- @endempty --}}
 
 
                                 </div>
-                                <div id="menu1" class="container tab-pane fade"><br>
-                                    <h3>Menu 1</h3>
-                                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                        aliquip ex ea commodo consequat.</p>
+                                <div id="menu1"
+                                    class="container tab-pane {{ $payment_stat == 'not_paid' ? 'active' : 'fade' }}"><br>
+
+                                    <form action="{{ route('admin.installments.filter1') }}" method="get">
+                                        @csrf
+                                        <div class="row ">
+
+                                            <div class="col-md-6 col-12 d-flex justify-content-around">
+
+                                                <h4>
+                                                    فیلتر بر اساس شماره تلفن
+                                                </h4>
+
+                                                <div class="d-flex">
+                                                    <input type="text" name="filter1" class="form-control w-auto mr-1"
+                                                        placeholder="شماره تلفن را برای فیلتر وارد کنید">
+                                                    <input type="submit" class="btn btn-info" value="فیلتر">
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6 col-12">
+                                            </div>
+                                        </div>
+                                    </form>
+
+
+
+                                    <div class="row mt-1 ml-2">
+                                        <h3>
+                                            لیست اقساط پرداخت نشده:
+                                        </h3>
+                                    </div>
+
+                                    @if (empty($installments1))
+                                        <div class="row mt-3 ml-2">
+                                            <h4>
+                                                هیچ قسطی برای نمایش وجود ندارد!
+                                            </h4>
+                                        </div>
+                                    @else
+                                        @foreach ($installments1 as $key)
+                                            @if ($key->status == 1)
+                                                {{-- <div class="row mt-3 ml-2">
+                                                    <h4>
+                                                        اقساط فروشگاه: {{ $key->seller->first_name }}
+                                                    </h4>
+                                                </div> --}}
+
+
+
+                                                <div class="border rounded p-2 my-1">
+                                                    <div class="row d-flex justify-content-center">
+                                                        <h5>قسط آقای: {{ $key->user->username }}
+                                                        </h5>
+                                                    </div>
+
+
+                                                    <div class="row">
+                                                        مبلغ کل فروش:{{ $key->Creditamount }}
+                                                    </div>
+                                                    <div class="row">
+                                                        {{ $key->numberofinstallments }} عدد قسط به سر رسیده
+                                                        ۶ هر ماه به مبلغ قسط
+                                                        {{ $key->amounteachinstallment }} ریال
+                                                    </div>
+
+                                                    <div class="row mt-2">
+                                                        <div class="col">
+                                                            مقدار پیش پرداخت {{ $key->prepaidamount }} ریال
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            @endif
+                                        @endforeach
+                                    @endif
                                 </div>
-                                <div id="menu2" class="container tab-pane fade"><br>
+                                <div id="menu2"
+                                    class="container tab-pane {{ $payment_stat == 'paid' ? 'active' : 'fade' }}"><br>
                                     <div class="row">
 
                                         <div class="col-md-6 col-12">
