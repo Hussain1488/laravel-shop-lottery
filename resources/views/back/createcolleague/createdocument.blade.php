00a6fc8ff4c5ba62ac1e0c8555863659271e7225
hussain1488 <hedayati1488@gmail.com>
2023-10-28 14:54:14 +0330

    createdocument.blade has been designed.

--- resources/views/back/createcolleague/createdocument.blade.php
+++ resources/views/back/createcolleague/createdocument.blade.php
@@ -25,8 +25,126 @@
 
             </div>
             <div class="content-body">
-         fgfdg
+                <section class="card">
+                    <div class="card-header">
+                    </div>
+                    <div class="card-content">
+                        <h6 class="card-title m-2">سند جدید</h6>
+                        <div class="container mt-3">
+
+
+                            <form action="{{ route('admin.colleagueCredit.store') }}" method="POST"
+                                enctype="multipart/form-data">
+                                @csrf
+
+
+                                <div class="tab-content">
+                                    <div id="home" class="container tab-pane active"><br>
+
+
+                                        <div class="row">
+                                            <div class="col-md-3 col-6 pt-2">
+                                                <h5>
+                                                    نام بده کار
+                                                </h5>
+                                            </div>
+                                            <div class="col-md-3 col-6">
+                                                <div class="form-group">
+                                                    <label>سرچ بر اساس شماره تلفن</label>
+                                                    <select type="text" class="form-control" name="userselected">
+                                                        {{-- @foreach ($users as $item)
+                                                            <option value="{{ $item->id }}">{{ $item->username }}
+                                                            </option>
+                                                        @endforeach --}}
+                                                        <option value="">Hussian</option>
+
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-3 col-6 pt-2">
+                                                <h5>
+                                                    نام بستانکار
+                                                </h5>
+                                            </div>
+                                            <div class="col-md-3 col-6">
+                                                <div class="form-group">
+                                                    <label>سرچ بر اساس شماره تلفن</label>
+                                                    <select type="text" class="form-control" name="userselected">
+                                                        {{-- @foreach ($users as $item)
+                                                            <option value="{{ $item->id }}">{{ $item->username }}
+                                                            </option>
+                                                        @endforeach --}}
+                                                        <option value="">Hussian</option>
+
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+
+                                            <div class="col-md-3 col-6 pt-2">
+                                                <div class="form-group d-flex align-items-center">
+                                                    <h5 for="purchasecredit" class="mr-2">
+                                                        مبلغ به ریال وارد شود
+                                                    </h5>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3 col-6 pt-2">
+                                                <div class="d-flex align-items-center">
+                                                    <input type="text" placeholder="100,000"
+                                                        class="form-control moneyInput" id="first_name"
+                                                        name="purchasecredit" style="margin-left: 4px;">
+                                                    <span>ریال</span>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-3 col-6 pt-2">
+                                                <div class="form-group d-flex align-items-center">
+                                                    <h5 for="documents" class="mr-2">
+                                                        آپلود مدارک
+                                                    </h5>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3 col-6 pt-2">
+                                                <div class="d-flex align-items-center">
+                                                    <input multiple type="file" class="form-control mt-1 mr-1"
+                                                        name="documents[]">
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+                                        <div class="row my-2">
+                                            <div class="col-md-6 col-12 pt-2">
+                                                <h5>
+                                                    شماره ثبت این سند به شماره ۹۸۵۶۳ میباشد.
+                                                </h5>
+                                                <h5>
+                                                    شماره پیگیری سند در سند اداری ثبت شده و تحویل بایگانی شود.</h5>
+                                            </div>
+
+                                        </div>
+
+                                        <div class="row ">
+                                            <div class="col d-flex align-items-baseline justify-content-center">
+                                                <input type="submit"
+                                                    class="btn btn-primary my-1"value=" تأیید نهایی تغییرات" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </form>
+
+                        </div>
+                </section>
+
             </div>
+
         </div>
     </div>
 @endsection
