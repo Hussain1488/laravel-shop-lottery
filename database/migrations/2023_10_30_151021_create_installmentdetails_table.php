5d909ccdb8e719b6cf6583952252cc9e1ccb57cf
Emad Akroush <emadakroush@gmail.com>
2023-10-30 04:53:22 -0700

    add table installmentdetails

---
+++ database/migrations/2023_10_30_151021_create_installmentdetails_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInstallmentdetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('installmentdetails', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('installment_id')->nullable();
+            $table->foreign('installment_id')->references('id')->on('makeinstallmentsms')->onDelete('cascade');
+            $table->date("duedate");
+            $table->bigInteger("installmentnumber");
+            $table->string("installmentprice");
+            $table->boolean("paymentstatus");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('installmentdetails');
+    }
+}
