8eb0878b94aa64c40572769b1445e82287a9031b
hussain1488 <hedayati1488@gmail.com>
2023-11-07 10:47:13 +0330

    clearing store request created.

---
+++ app/Http/Requests/Back/ClearingStoreRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Back;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClearingStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "depositamount" => "required|numeric",
+            "shabanumber" => "required|numeric",
+            "factor" => "file",
+        ];
+    }
+    public function attributes()
+    {
+        return [
+            "" => "",
+        ];
+    }
+}
