6bbc9ae95d4df4f0917dc2fac780ac7d9db987d4
Emad Akroush <emadakroush@gmail.com>
2023-11-08 01:11:39 -0800

    add createstoretransactions table

---
+++ database/migrations/2023_11_08_114656_create_createstoretransactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCreatestoretransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('createstoretransactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('store_id');
+            $table->foreign('store_id')->references('id')->on('createstores')->onDelete('cascade');;
+            $table->integer('flag');
+            $table->date('datetransaction');
+            $table->string('typeoftransaction');
+            $table->bigInteger('price');
+            $table->bigInteger('finalprice');
+            $table->bigInteger('documentnumber');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('createstoretransactions');
+    }
+}
