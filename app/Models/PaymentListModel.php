62df9ba480ab49593bcc5f1af2a4d4cfa8d996dd
hussain1488 <hedayati1488@gmail.com>
2023-11-09 13:25:15 +0330

    paymentlist model updated. final_price add.

--- app/Models/PaymentListModel.php
+++ app/Models/PaymentListModel.php
@@ -10,5 +10,5 @@ class PaymentListModel extends Model
     use HasFactory;
 
     protected $table = 'list_of_payment';
-    protected $fillable = ['store_id', 'list_id', 'depositamount', 'shabanumber', 'factor', 'depositdate', 'status'];
+    protected $fillable = ['store_id', 'list_id', 'depositamount', 'shabanumber', 'factor', 'depositdate', 'status', 'final_price'];
 }
