7153f7b34bdbe88ad84e8427fe4a53235a254399
hussain1488 <hedayati1488@gmail.com>
2023-11-11 11:17:17 +0330

    pay deatils model created and list of payment relation defined

--- app/Models/paymentdetails.php
+++ app/Models/paymentdetails.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class paymentdetails extends Model
 {
     use HasFactory;
+
+    protected $table = 'paymentdetails';
+    protected $fillable = [
+        'list_of_payment_id',
+        'Issuetracking',
+        'nameofbank',
+        'documentpayment',
+    ];
+
+    public function payments()
+    {
+        return $this->belongsTo(PaymentListModel::class, 'list_of_payment_id');
+    }
 }
