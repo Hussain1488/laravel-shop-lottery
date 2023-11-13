179ec9ff8f8ebeeb68e3d69d5237bf7f2436c765
hussain1488 <hedayati1488@gmail.com>
2023-11-13 16:52:37 +0330

    buyer transaction migration created.

---
+++ database/migrations/2023_11_13_145950_create_buyertransactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuyertransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buyertransactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');;
+            $table->integer('flag');
+            $table->date('datetransaction');
+            $table->string('typeoftransaction');
+            $table->bigInteger('price');
+            $table->bigInteger('finalprice');
+            $table->bigInteger('documentnumber');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buyertransactions');
+    }
+}
