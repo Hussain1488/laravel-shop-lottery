8f99ae5d009cbf716a25bd49edf30c0649fa7bd5
hussain1488 <hedayati1488@gmail.com>
2023-11-13 16:55:06 +0330

    installmentListcontroller created for front

---
+++ themes/DefaultTheme/src/Controllers/installmentsListController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Themes\DefaultTheme\src\Controllers;
+
+use App\Models\installmentdetails;
+use App\Models\Makeinstallmentsm;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Events\OrderPaid;
+use App\Events\WalletAmountIncreased;
+use App\Http\Controllers\Controller;
+use App\Models\BankAccount;
+use App\Models\banktransaction;
+use App\Models\buyertransaction;
+use App\Models\createstore;
+use App\Models\Gateway;
+use App\Models\Transaction;
+use App\Models\User as ModelsUser;
+use App\Models\WalletHistory;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Morilog\Jalali\Jalalian;
+use Shetabit\Payment\Facade\Payment;
+use Shetabit\Multipay\Invoice;
+
+class installmentsListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function purchaseList()
+    {
+        $trans = buyertransaction::get();
+        return view('front::user.installments.installment_list', compact('trans'));
+    }
+}
