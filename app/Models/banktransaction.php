d1f6fdcee2131923cfd3b21e15027995ac9e8d02
hussain1488 <hedayati1488@gmail.com>
2023-11-15 10:46:30 +0330

    transaction method added

--- app/Models/banktransaction.php
+++ app/Models/banktransaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Morilog\Jalali\Jalalian;
 
 class banktransaction extends Model
 {
@@ -16,5 +17,31 @@ class banktransaction extends Model
     public function bank()
     {
         return $this->belongsTo(BankAccount::class, 'bank_id');
+    }
+
+    public function transaction($bank_id, $creditAmount, $status, $trans_id)
+    {
+
+        $recordCount = banktransaction::where('bank_id', $bank_id)->count();
+        if ($recordCount > 0) {
+            $lastRecord = banktransaction::where('bank_id', $bank_id)->latest()->first();
+            $bank = banktransaction::create([
+                'bank_id' => $bank_id,
+                'bankbalance' => $status ? $lastRecord->bankbalance + $creditAmount : $lastRecord->bankbalance - $creditAmount,
+                'transactionprice' => $creditAmount,
+                'transactionsdate' => Jalalian::now()->format('Y-m-d'),
+                'buyer_trans_id' => $trans_id
+
+            ]);
+        } else {
+            $bank = banktransaction::create([
+                'bank_id' => $bank_id,
+                'bankbalance' => $status ? $creditAmount : -$creditAmount,
+                'transactionprice' => $creditAmount,
+                'transactionsdate' => Jalalian::now()->format('Y-m-d'),
+                'buyer_trans_id' => $trans_id
+            ]);
+        }
+        return $bank;
     }
 }
--- app/Models/buyertransaction.php
+++ app/Models/buyertransaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Morilog\Jalali\Jalalian;
 
 class buyertransaction extends Model
 {
@@ -16,5 +17,48 @@ class buyertransaction extends Model
     public function user()
     {
         return  $this->belongsTo(User::class);
+    }
+
+    public function transaction($user, $amount, $status)
+    {
+        $user_transaction_number = buyertransaction::count();
+        if ($user_transaction_number > 0) {
+            $doc_number = buyertransaction::latest()->first()->documentnumber + 1;
+            if (buyertransaction::where('flag', 1)->where('user_id', $user->id)->count() > 0) {
+                if ($status) {
+                    $final_price = buyertransaction::where('flag', 1)->where('user_id', $user->id)->latest()->first()->finalprice + $amount;
+                } else {
+                    $final_price = buyertransaction::where('flag', 1)->where('user_id', $user->id)->latest()->first()->finalprice - $amount;
+                }
+            } else {
+                if ($status) {
+
+                    $final_price = +$amount;
+                } else {
+
+                    $final_price = -$amount;
+                }
+            }
+        } else {
+            $doc_number = 10000;
+            if ($status) {
+
+                $final_price = +$amount;
+            } else {
+
+                $final_price = -$amount;
+            }
+        }
+
+        $user_trans = buyertransaction::create([
+            'user_id' => $user->id,
+            'flag' => 1,
+            'datetransaction' => Jalalian::now(),
+            'typeoftransaction' => 0,
+            'price' => $amount,
+            'finalprice' => $final_price,
+            'documentnumber' => $doc_number
+        ]);
+        return $user_trans;
     }
 }
