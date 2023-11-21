fe7eea3f44da689be2b8637aab56539e3184aeab
hussain1488 <hedayati1488@gmail.com>
2023-11-21 11:16:49 +0330

    recharge input changed to money format

--- public/front/script.js
+++ public/front/script.js
@@ -12,6 +12,19 @@ $(document).ready(function () {
         $(this).text(formattedNumber);
     });
 
+    $('.moneyInput').on('input', function () {
+        var input = $(this).val();
+
+        // Remove any non-digit characters (e.g., commas)
+        var digits = input.replace(/\D/g, '');
+
+        // Format the number with commas
+        var formattedNumber = addCommas(digits);
+
+        // Update the input field with the formatted number
+        $(this).val(formattedNumber);
+    });
+
     $('.moneyInput').each(function () {
         var input = $(this).val();
 
@@ -26,18 +39,19 @@ $(document).ready(function () {
     });
 
     // Function to add commas as a thousands separator
-    function addCommas(nStr) {
-        nStr += '';
-        var x = nStr.split('.');
-        var x1 = x[0];
-        var x2 = x.length > 1 ? '.' + x[1] : '';
-        var rgx = /(\d+)(\d{3})/;
-        while (rgx.test(x1)) {
-            x1 = x1.replace(rgx, '$1' + ',' + '$2');
-        }
-        return x1 + x2;
-    }
     $('.activation-code-input').activationCodeInput({
         number: 5
     });
 });
+
+function addCommas(nStr) {
+    nStr += '';
+    var x = nStr.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+}
