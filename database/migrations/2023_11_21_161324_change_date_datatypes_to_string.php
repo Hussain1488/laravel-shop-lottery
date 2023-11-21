c6a0852159d03e655a0c9fcd731535144c989c71
hussain1488 <hedayati1488@gmail.com>
2023-11-21 17:13:31 +0330

    persian date type changed to string type

---
+++ database/migrations/2023_11_21_161324_change_date_datatypes_to_string.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeDateDatatypesToString extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('buyertransactions', function (Blueprint $table) {
+            $table->string('datetransaction')->change();
+        });
+        Schema::table('createstoretransactions', function (Blueprint $table) {
+            $table->string('datetransaction')->change();
+        });
+        Schema::table('list_of_payment', function (Blueprint $table) {
+            $table->string('depositdate')->change();
+        });
+        Schema::table('banktransactions', function (Blueprint $table) {
+            $table->string('transactionsdate')->change();
+        });
+        Schema::table('installmentdetails', function (Blueprint $table) {
+            $table->string('duedate')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('string', function (Blueprint $table) {
+            //
+        });
+    }
+}
