a0ddb91a33666e2aa6b4b5a9c6c45aac52ce3907
hussain1488 <hedayati1488@gmail.com>
2023-11-25 20:42:33 +0330

    login regester whith otp

--- app/Http/Controllers/Auth/LoginWithCodeController.php
+++ app/Http/Controllers/Auth/LoginWithCodeController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Validation\Rule;
 
 class LoginWithCodeController extends Controller
@@ -25,18 +26,51 @@ class LoginWithCodeController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'mobile' => 'required|exists:users,username',
-            'captcha' => ['required', 'captcha'],
-        ], [
-            'mobile.exists' => 'حساب کاربری با شماره موبایل ' . $request->mobile . ' وجود ندارد. لطفا ثبت نام کنید'
-        ]);
+        // $request->validate([
+        //     'mobile' => 'required|exists:users,username',
+        //     'captcha' => ['required', 'captcha'],
+        // ], [
+        //     'mobile.exists' => 'حساب کاربری با شماره موبایل ' . $request->mobile . ' وجود ندارد. لطفا ثبت نام کنید'
+        // ]);
 
+        $type = new class
+        {
+            public $code;
+            public $text;
+        };
         $user = User::where('username', $request->mobile)->first();
 
-        PasswordResetLinkController::sendCode($user);
+        if ($user) {
+            PasswordResetLinkController::sendCode($user);
 
-        return response('success');
+            return response()->json(['status' => 'success', 'data' => 'login']);
+        } else {
+            // Create a new user
+            $user = new User();
+            $user->username = $request->mobile;
+
+            $type = new \stdClass(); // Creating a new instance of stdClass
+            $type->code = rand(11111, 99999);
+            $type->text = 'کد ثبت نام:';
+
+            // Assuming verifySms is a function that expects $type and $user as arguments
+            verifySms($type, $user);
+
+            Session::put('newUser', ['number' => $request->mobile, 'code' => $type->code]);
+
+            return response()->json(['status' => 'success', 'data' => 'register']);
+        }
+        // } else {
+        //     $user = new User();
+        //     $user->username = $request->mobile;
+        //     $type->code = rand(11111, 99999);
+        //     $type->text = 'کد ثبت نام:';
+        //     verifySms($type, $user);
+
+        //     Session::put('newUser', ['number' => $request->mobile, 'code' => $type->code]);
+
+        //     return response('success');
+        // }
     }
 
     public function confirm(Request $request)
