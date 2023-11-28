6a496157231b655cc1ffd5cf5b0f42c756c9f34d
hussain1488 <hedayati1488@gmail.com>
2023-11-28 12:16:58 +0330

    otp file added to sms varification views

--- public/back/assets/js/pages/login.js
+++ public/back/assets/js/pages/login.js
@@ -1,8 +1,25 @@
-$(document).ready(function() {
+var inter = 'pass';
+var loading = {
+    message:
+        '<div class="loadingio-spinner-eclipse-mbdtacxsn3d"> ' +
+        '<div class="ldio-ffthf4779sc">' +
+        '<div></div>' +
+        '</div>' +
+        '</div>',
+    css: {
+        width: 'auto' /* Auto width for the blockUI container */,
+        top: '50%' /* Center vertically */,
+        left: '50%' /* Center horizontally */,
+        transform: 'translate(-50%, -50%)' /* Center the element itself */,
+        backgroundColor: 'transparent' /* No background color */,
+        border: 'none' /* No border */,
+        color: '#333' /* Text color */
+    }
+};
 
-    $('#login-form').submit(function(e) {
+$(document).ready(function () {
+    $('#login-form').submit(function (e) {
         e.preventDefault();
-
 
         var formData = new FormData(this);
 
@@ -10,16 +27,19 @@ $(document).ready(function() {
             url: $(this).attr('action'),
             type: 'POST',
             data: formData,
-            success: function(data) {
+            success: function (data) {
                 if ($.isEmptyObject(data.error)) {
                     window.location.href = redirect_url;
                 }
             },
-            beforeSend: function(xhr) {
+            beforeSend: function (xhr) {
                 block('#main-card');
-                xhr.setRequestHeader("X-CSRF-TOKEN", $('meta[name="csrf-token"]').attr('content'));
+                xhr.setRequestHeader(
+                    'X-CSRF-TOKEN',
+                    $('meta[name="csrf-token"]').attr('content')
+                );
             },
-            complete: function() {
+            complete: function () {
                 unblock('#main-card');
             },
 
@@ -28,4 +48,33 @@ $(document).ready(function() {
             processData: false
         });
     });
+});
+
+$('#sms-verify').on('click', function () {
+    if ($(this).text() == 'ورود با پیامک') {
+        $('#flag').val('sms');
+        $('#pass-verify-fieldset').addClass('d-none');
+        $('#sms-verify-fieldset').removeClass('d-none');
+        $(this).text(' ورود با رمز عبور');
+        inter = 'sms';
+        block('#main-card');
+        $.ajax({
+            url: sms_varify,
+            type: 'GET',
+            success: function () {
+                
+                unblock('#main-card');
+            }
+        });
+        // $.blockUI(loading);
+    } else {
+        $('#sms-verify-fieldset').addClass('d-none');
+        $('#pass-verify-fieldset').removeClass('d-none');
+        $(this).text('ورود با پیامک');
+        $('#flag').val('pass');
+        inter = 'pass';
+    }
+});
+$('#inter-button').on('click', function () {
+    $('#login-form').submit();
 });
