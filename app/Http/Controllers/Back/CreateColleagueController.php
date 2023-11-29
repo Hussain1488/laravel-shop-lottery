b04630efe8ff99cc097cfcf9dc1ab8b76e0b4d21
hussain1488 <hedayati1488@gmail.com>
2023-11-29 16:10:16 +0330

    store credite transaction show debugged.

--- app/Http/Controllers/Back/CreateColleagueController.php
+++ app/Http/Controllers/Back/CreateColleagueController.php
@@ -191,7 +191,7 @@ class CreateColleagueController extends Controller
                 'account_id' => $request->account_id,
                 'conrn_job_reccredite' => $storecredit,
             ]);
-            $trans_id = createstoretransaction::storeTransaction($store, $storecredit, true, 0, 1);
+            $trans_id = createstoretransaction::storeTransaction($store, $storecredit, true, 3, 0);
 
             $bankt_tras = banktransaction::transaction($bank_id->id, $storecredit, true, $trans_id, 'store');
 
@@ -327,7 +327,7 @@ class CreateColleagueController extends Controller
 
         OperatorActivity::createActivity($store->user->id, 'STORE_CREDIT');
 
-        $trans_id = createstoretransaction::storeTransaction($store, $request->storecredit, true, 1, 0);
+        $trans_id = createstoretransaction::storeTransaction($store, $request->storecredit, true, 3, 0);
 
 
         $bank_trans = banktransaction::transaction($bank_id->id, $request->storecredit, false, $trans_id, 'store');
