58fb41a984cbb7da55a3a86319f3fb45d6b9ec35
hussain1488 <hedayati1488@gmail.com>
2023-12-03 12:09:09 +0330

    code added.

--- database/seeders/BankTypeSeeder.php
+++ database/seeders/BankTypeSeeder.php
@@ -14,10 +14,14 @@ class BankTypeSeeder extends Seeder
      */
     public function run()
     {
-        $names = ['بانک', 'درآمد', 'هزینه', 'واسط قسط ها', 'واسط اعتبار فروش فروشگاه ها', 'مقدار اعتبار خرید خریدارها'];
+        $names = ['بانک',  'هزینه', 'درآمد', 'واسط قسط ها', 'واسط اعتبار فروش فروشگاه ها', 'مقدار اعتبار خرید خریدارها'];
+        // $codes = [21,22,23,24,25,26];
+        $code = 21;
         foreach ($names as $name) {
             bankTypeModel::create([
-                "name" => $name
+                "name" => $name,
+                "code" => $code++
+
             ]);
         }
     }
