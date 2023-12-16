597f19437f60457682853659a4093f1836182e2f
hussain1488 <hedayati1488@gmail.com>
2023-12-16 17:19:23 +0330

    adding relation with storetransaction and pay list

--- app/Models/PaymentListModel.php
+++ app/Models/PaymentListModel.php
@@ -10,8 +10,12 @@ class PaymentListModel extends Model
     use HasFactory;
 
     protected $table = 'list_of_payment';
-    protected $fillable = ['store_id', 'list_id', 'depositamount', 'shabanumber', 'factor', 'depositdate', 'status', 'final_price'];
+    protected $fillable = ['store_id', 'list_id', 'depositamount', 'shabanumber', 'factor', 'depositdate', 'status', 'final_price', 'trans_id '];
 
+    public function transaction()
+    {
+        return $this->belongsTo(createstoretransaction::class, 'trans_id');
+    }
     public function store()
     {
         return $this->belongsTo(createstore::class, 'store_id');
