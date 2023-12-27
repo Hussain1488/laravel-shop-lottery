6ed54e30a006a98479379f30075ed1ead9b5eafe
hussain1488 <hedayati1488@gmail.com>
2023-12-27 12:28:47 +0330

    Revert "relation with bankAccount defined."
    This reverts commit 6d40b6d28d2c8e835ddb1dc195e8fba5fcfaea5e.

--- app/Models/paymentdetails.php
+++ app/Models/paymentdetails.php
@@ -13,16 +13,12 @@ class paymentdetails extends Model
     protected $fillable = [
         'list_of_payment_id',
         'Issuetracking',
-        'bank_id',
+        'nameofbank',
         'documentpayment',
     ];
 
     public function payments()
     {
         return $this->belongsTo(PaymentListModel::class, 'list_of_payment_id');
-    }
-    public function bank()
-    {
-        return $this->belongsTo(BankAccount::class, 'bank_id');
     }
 }
