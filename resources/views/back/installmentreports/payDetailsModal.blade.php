22c042043441d81d1ec60a8469b58e60bdf24bdc
hussain1488 <hedayati1488@gmail.com>
2023-12-31 12:11:44 +0330

    details modal created.

---
+++ resources/views/back/installmentreports/payDetailsModal.blade.php
@@ -0,0 +1,27 @@
+ <div class="modal fade" id="payListDetails">
+     <div class="modal-dialog modal-dialog-centered">
+         <div class="modal-content">
+
+             <!-- Modal Header -->
+             <div class="modal-header">
+                 <h4 class="modal-title">جزئیات تسویه حساب:<span class="text-success" id="deposit_amount_show"></span>
+                 </h4>
+             </div>
+             <hr />
+
+             <!-- Modal body -->
+             <div class="modal-body p-2">
+
+
+             </div>
+
+             <!-- Modal footer -->
+
+             <div class="modal-footer">
+
+             </div>
+             <button type="button" class="btn btn-danger m-1 d-flex justify-content-center" data-dismiss="modal"><i
+                     class="feather icon-x-circle" style=""></i> بستن</button>
+         </div>
+     </div>
+ </div>
