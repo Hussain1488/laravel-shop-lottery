a012cda657ca26167534d587c86e91f1ff4b8355
hussain1488 <hedayati1488@gmail.com>
2024-01-01 14:15:25 +0330

    responsive view updated.

--- resources/views/back/createcolleague/show.blade.php
+++ resources/views/back/createcolleague/show.blade.php
@@ -49,23 +49,23 @@
 
                                 <div class="mr-1 mt-1">
                                     <a href="{{ route('admin.cooperationsales.mainWallet', [$store->id]) }}"
-                                        class="btn btn-info">کیف پول
+                                        class="btn btn-info btn-sm font-bold">کیف پول
                                         اصلی</a>
                                 </div>
                                 <div class="mr-1 mt-1">
 
                                     <a href="{{ route('admin.cooperationsales.payRequestWallet', [$store->id]) }}"
-                                        class="btn btn-info">کیف پول درخواست واریز</a>
+                                        class="btn btn-info btn-sm font-bold">کیف پول درخواست واریز</a>
                                 </div>
                                 <div class="mr-1 mt-1">
 
                                     <a href="{{ route('admin.cooperationsales.paidSales', [$store->id]) }}"
-                                        class="btn btn-info">فروش های تسویه شده</a>
+                                        class="btn btn-info btn-sm font-bold">فروش های تسویه شده</a>
                                 </div>
                                 <div class="mr-1 mt-1">
 
                                     <a href="{{ route('admin.cooperationsales.creditTrans', [$store->id]) }}"
-                                        class="btn btn-info">تراکنش های اعتبار فروشگاه</a>
+                                        class="btn btn-info btn-sm font-bold">تراکنش های اعتبار فروشگاه</a>
                                 </div>
                                 {{-- <a href="mr-1 mt-1" class="btn btn-success">مقدار فروش اقساط ماهانه</a> --}}
                             </div>
@@ -73,145 +73,138 @@
 
 
                         <!-- Tab panes -->
-                        <div class="mb-3 mx-2 ">
-                            <div class="row d-flex justify-center mt-2">
 
-                                <div class="col-12 col-lg-6 col-md-6 col-lx-6 col-sm-12">
-                                    <div class="row">
-                                        <div class="col">
+                        <div class="mb-3 mx-1 p-1 mt-2 border-2 rounded-2xl">
+                            <div class="row  ">
 
-                                            <h5 style="font-size: 18px" for="inventory" class="mr-2">
-                                                آدرس فروشگاه :
-                                            </h5>
-                                        </div>
-                                        <div class="col">
-                                            <span style="font-size: 18px">{{ $store->addressofstore }}</span>
-                                        </div>
-                                    </div>
+                                <div class="col-12 col-lg-3 col-md-6 col-lx-3 col-sm-12 mt-1">
+
+                                    <h5 style="font-size: 16px" for="inventory" class="mr-2">
+                                        آدرس فروشگاه :
+                                    </h5>
+                                </div>
+                                <div class="col-12 col-lg-3 col-md-6 col-lx-3 col-sm-12 pl-3">
+                                    <span style="font-size: 14px">{{ $store->addressofstore }}</span>
+
                                 </div>
 
-                                <div class="col-12 col-lg-6 col-md-6 col-lx-6 col-sm-12">
-                                    <div class="row">
-                                        <div class="col">
 
-                                            <h5 style="font-size: 18px" for="inventory" class="mr-2">
-                                                مسئول فروشگاه :
-                                            </h5>
-                                        </div>
-                                        <div class="col">
-                                            <span style="font-size: 18px">{{ $store->user->username }}</span>
-                                        </div>
-                                    </div>
+                                <div class="col-12 col-lg-3 col-md-6 col-lx-3 col-sm-12 mt-1">
+
+                                    <h5 style="font-size: 16px" for="inventory" class="mr-2">
+                                        مسئول فروشگاه :
+                                    </h5>
                                 </div>
-
-                            </div>
-                            <div class="row d-flex justify-center mt-2">
-
-                                <div class="col-12 col-lg-6 col-md-6 col-lx-6 col-sm-12">
-                                    <div class="row">
-                                        <div class="col">
-
-                                            <h5 style="font-size: 18px" for="inventory" class="mr-2">
-                                                مقدار کارمز فروشگاه:
-                                            </h5>
-                                        </div>
-                                        <div class="col">
-                                            <span style="font-size: 18px">{{ $store->feepercentage }}٪</span>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-12 col-lg-6 col-md-6 col-lx-6 col-sm-12">
-                                    <div class="row">
-                                        <div class="col">
-
-                                            <h5 style="font-size: 18px" for="inventory" class="mr-2">
-                                                اعتبار فروشگاه:
-                                            </h5>
-                                        </div>
-                                        <div class="col">
-                                            <span style="font-size: 18px"
-                                                class="monyInputSpan">{{ $store->storecredit }}</span> ریال
-                                        </div>
-                                    </div>
+                                <div class="col-12 col-lg-3 col-md-6 col-lx-3 col-sm-12 pl-3">
+                                    <span style="font-size: 14px">{{ $store->user->username }}</span>
                                 </div>
 
                             </div>
 
-                            <div class="row d-flex justify-center mt-2">
 
-                                <div class="col-6 col-lg-6 col-md-6 col-lx-6 col-sm-12 ">
-                                    <div class="row">
-                                        <div class="col">
+                            <div class="row ">
 
-                                            <h5 style="font-size: 18px" for="inventory" class="mr-2">
-                                                مقدار فروش فروشگاه:
-                                            </h5>
-                                        </div>
-                                        <div class="col">
-                                            <span style="font-size: 18px"
-                                                class="monyInputSpan">{{ $store->salesamount }}</span> ریال
-                                        </div>
-                                    </div>
+                                <div class="col-12 col-lg-3 col-md-6 col-lx-3 col-sm-12 mt-1">
+
+                                    <h5 style="font-size: 16px" for="inventory" class="mr-2">
+                                        مقدار کارمز فروشگاه:
+                                    </h5>
                                 </div>
 
-                                <div class="col-6 col-lg-6 col-md-6 col-lx-6 col-sm-12">
-                                    <div class="row">
-                                        <div class="col">
 
-                                            <h5 style="font-size: 18px" for="inventory" class="mr-2">
-                                                اسناد :
-                                            </h5>
-                                        </div>
-                                        <div class="col">
-                                            <div class="row d-flex flex-wrap">
+                                <div class="col-12 col-lg-3 col-md-6 col-lx-3 col-sm-12 pl-3">
+                                    <span style="font-size: 14px">{{ $store->feepercentage }}٪</span>
 
-                                                @php
-                                                    use Illuminate\Support\Str;
-                                                    $counter = 1;
-                                                @endphp
-                                                @isset($doc)
-                                                    @if (count($doc) > 1)
-                                                        <div class="col-12">
-                                                            <a href="{{ route('admin.createcolleague.fileDownload', [$store->id]) }}"
-                                                                class="btn btn-primary">
-                                                                دانلود همه فایل ها<i class="feather icon-download"></i>
+                                </div>
+
+
+
+                                <div class="col-12 col-lg-3 col-md-6 col-lx-3 col-sm-12 mt-1">
+
+                                    <h5 style="font-size: 16px" for="inventory" class="mr-2">
+                                        اعتبار فروشگاه:
+                                    </h5>
+                                </div>
+                                <div class="col-12 col-lg-3 col-md-6 col-lx-3 col-sm-12 pl-3">
+                                    <span style="font-size: 14px" class="monyInputSpan">{{ $store->storecredit }}</span>
+                                    ریال
+
+                                </div>
+                            </div>
+
+
+
+                            <div class="row ">
+
+
+                                <div class="col-12 col-lg-3 col-md-6 col-lx-3 col-sm-12 mt-1">
+
+
+                                    <h5 style="font-size: 16px" for="inventory" class="mr-2">
+                                        مقدار فروش فروشگاه:
+                                    </h5>
+                                </div>
+                                <div class="col-12 col-lg-3 col-md-6 col-lx-3 col-sm-12 pl-3">
+                                    <span style="font-size: 14px" class="monyInputSpan">{{ $store->salesamount }}</span>
+                                    ریال
+
+                                </div>
+
+
+                                <div class="col-12 col-lg-3 col-md-6 col-lx-3 col-sm-12 mt-1">
+
+                                    <h5 style="font-size: 16px" for="inventory" class="mr-2">
+                                        اسناد :
+                                    </h5>
+                                </div>
+                                <div class="col-12 col-lg-3 col-md-6 col-lx-3 col-sm-12 pl-1">
+
+                                    <div class="row d-flex flex-wrap">
+                                        @php
+                                            use Illuminate\Support\Str;
+                                            $counter = 1;
+                                        @endphp
+                                        @isset($doc)
+                                            @if (count($doc) > 1)
+                                                <div class="col-12">
+                                                    <a href="{{ route('admin.createcolleague.fileDownload', [$store->id]) }}"
+                                                        class="btn btn-primary">
+                                                        دانلود همه فایل ها<i class="feather icon-download"></i>
+                                                    </a>
+                                                </div>
+                                            @endif
+
+
+                                            @foreach ($doc as $document)
+                                                @if (Str::endsWith($document, ['.jpg', '.jpeg', '.png', '.gif', '.bmp']))
+                                                    <div class="mt-1 mr-1">
+                                                        <div style="position: relative; display: inline-block; width:100px">
+                                                            <a style="position: absolute; top: 5%; right: 5%;"
+                                                                class="badge badge-secondary badge-pill"
+                                                                href="{{ asset($document) }}" download="{{ $document }}">
+                                                                <span class="badge badge-success badge-pill"><i
+                                                                        class="feather icon-download"></i></span>
+                                                                دانلود
                                                             </a>
+                                                            <img src="{{ asset($document) }}" alt="Image">
                                                         </div>
-                                                    @endif
-
-
-                                                    @foreach ($doc as $document)
-                                                        @if (Str::endsWith($document, ['.jpg', '.jpeg', '.png', '.gif', '.bmp']))
-                                                            <div class="mt-1 mr-1">
-                                                                <div
-                                                                    style="position: relative; display: inline-block; width:100px">
-                                                                    <a style="position: absolute; top: 5%; right: 5%;"
-                                                                        class="badge badge-secondary badge-pill"
-                                                                        href="{{ asset($document) }}"
-                                                                        download="{{ $document }}">
-                                                                        <span class="badge badge-success badge-pill"><i
-                                                                                class="feather icon-download"></i></span>
-                                                                        دانلود
-                                                                    </a>
-                                                                    <img src="{{ asset($document) }}" alt="Image">
-                                                                </div>
-                                                            </div>
-                                                        @else
-                                                            <div class="mt-1 mr-1">
-                                                                <a class="btn btn-secondary" href="{{ asset($document) }}"
-                                                                    download="{{ $document }}"><span
-                                                                        class="badge badge-success badge-pill"><i
-                                                                            class="feather icon-download"></i></span> دانلود
-                                                                    فایل
-                                                                    {{ $counter++ }}</a>
-                                                            </div>
-                                                        @endif
-                                                    @endforeach
-                                                @endisset
-                                            </div>
-                                        </div>
+                                                    </div>
+                                                @else
+                                                    <div class="mt-1 mr-1">
+                                                        <a class="btn btn-secondary" href="{{ asset($document) }}"
+                                                            download="{{ $document }}"><span
+                                                                class="badge badge-success badge-pill"><i
+                                                                    class="feather icon-download"></i></span>
+                                                            دانلود
+                                                            فایل
+                                                            {{ $counter++ }}</a>
+                                                    </div>
+                                                @endif
+                                            @endforeach
+                                        @endisset
                                     </div>
+
+
                                 </div>
 
                             </div>
@@ -222,16 +215,15 @@
                         </div>
 
                     </div>
+                </section>
             </div>
+
+
+
+
         </div>
-
     </div>
 
-    </section>
-    </div>
-
-    </div>
-    </div>
 @endsection
 
 @include('back.partials.plugins', [
