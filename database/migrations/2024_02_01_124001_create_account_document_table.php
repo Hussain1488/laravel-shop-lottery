212a61eb14ae65264bd2b7c61580caab8cf5dd07
hussain1488 <hedayati1488@gmail.com>
2024-02-01 12:53:08 +0330

    bank document tabled has been defined.

---
+++ database/migrations/2024_02_01_124001_create_account_document_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountDocumentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account_document', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('debtor_trans_id')->index();
+            $table->unsignedBigInteger('creditor_trans_id')->index();
+            $table->text('documents')->nullable();
+            $table->enum('type', ['withdraw', 'deposit'])->nullable();
+            $table->bigInteger('numberofdocuments')->nullable();
+            $table->string('description', 710)->nullable();
+            $table->foreign('creditor_trans_id')->references('id')->on('banktransactions');
+            $table->foreign('debtor_trans_id')->references('id')->on('banktransactions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account_document');
+    }
+}
