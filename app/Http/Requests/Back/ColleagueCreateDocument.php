a90e16e360d8ff9bc45321f8ab3821861db9d695
hussain1488 <hedayati1488@gmail.com>
2024-02-07 16:09:05 +0330

    createdocument validation updated.

--- app/Http/Requests/Back/ColleagueCreateDocument.php
+++ app/Http/Requests/Back/ColleagueCreateDocument.php
@@ -25,20 +25,24 @@ class ColleagueCreateDocument extends FormRequest
     {
 
         return [
-            // "bank_id" => "required",
-            // "user_id" => "required",
-            // "ReCredintAmount" => "required",
-            // "documents" => "required",
-            // "description" => "required|max:700",
+            "debtor_type" => "required|numeric",
+            "debtor" => "required|numeric",
+            "creditor_type" => "required|numeric",
+            "creditor" => "required|numeric",
+            "amount" => "required",
+            "description" => "required|max:700",
+            "documents" => "required",
         ];
     }
     public function attributes()
     {
 
         return [
-            "bank_id" => "اسم بدهکار",
-            "user_id" => "اسم بستانکار",
-            "ReCredintAmount" => "مقدار اعتبار",
+            "debtor_type" => "ماهیت بدهکار",
+            "debtor" => "بدهکار",
+            "creditor_type" => "ماهیت بستانکار",
+            "creditor" => "بستانکار",
+            "amount" => "مبلغ سند مالی",
             "documents" => "اسناد",
             "description" => "توضیحات",
         ];
