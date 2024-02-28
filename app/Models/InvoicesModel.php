7361c923f44dda33c5400e8324be0039623a4843
hussain1488 <hedayati1488@gmail.com>
2024-02-28 16:15:02 +0330

    Defining invoicemodel

---
+++ app/Models/InvoicesModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InvoicesModel extends Model
+{
+    use HasFactory;
+    protected $table = 'invoices';
+    protected $fillable = ['user_id', 'amount', 'number'];
+}
