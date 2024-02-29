007b976cf090cf9ed213e73aa45bfb6e58bf881b
hussain1488 <hedayati1488@gmail.com>
2024-02-29 15:40:51 +0330

    new methods defined

--- app/Http/Controllers/lotteryController.php
+++ app/Http/Controllers/lotteryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DailyCodeModel;
 use Illuminate\Http\Request;
 
 class lotteryController extends Controller
@@ -10,5 +11,21 @@ class lotteryController extends Controller
     public function index()
     {
         return view('back.lottery.index');
+    }
+
+    public function invoiceDatatale()
+    {
+    }
+    public function invoiceDetails()
+    {
+    }
+    public function dailyCode()
+    {
+        $lastValue = DailyCodeModel::first()->date;
+        return view('back.lottery.dailyCode', compact('lastValue'));
+    }
+    public function generateCode()
+    {
+        return true;
     }
 }
