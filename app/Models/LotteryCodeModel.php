ed7a9d58c93122862edb835469426856b4199d0b
hussain1488 <hedayati1488@gmail.com>
2024-02-29 10:44:23 +0330

    relations defined.

--- app/Models/LotteryCodeModel.php
+++ app/Models/LotteryCodeModel.php
@@ -12,4 +12,24 @@ class LotteryCodeModel extends Model
     protected $table = 'lottery_codes';
 
     protected $fillable = ['user_id', 'code', 'daily_code_id', 'invoice_id', 'weekly_stat', 'monthly_stat'];
+
+    public function dailyCode()
+    {
+        return $this->belongsToMany(DailyCodeModel::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function winner()
+    {
+        return $this->hasMany(LotteryWinnersModel::class, 'lottery_code_id');
+    }
+
+    public function invoice()
+    {
+        return $this->belongsTo(InvoicesModel::class, 'invoice_id');
+    }
 }
