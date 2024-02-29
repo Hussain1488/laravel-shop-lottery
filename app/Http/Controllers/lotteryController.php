12e68283a46d550ea4a2fd2fa3818da5da972095
hussain1488 <hedayati1488@gmail.com>
2024-02-29 11:58:55 +0330

    lottery controller has been defined.

---
+++ app/Http/Controllers/lotteryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class lotteryController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('back.lottery.index');
+    }
+}
