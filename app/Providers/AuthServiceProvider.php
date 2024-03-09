9992ea0b69e24c9d0b5b86494c38d702c1d24207
hussain1488 <hedayati1488@gmail.com>
2024-03-09 13:57:16 +0330

    gates has been defined.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -43,6 +43,9 @@ class AuthServiceProvider extends ServiceProvider
         Gate::define('lottery.dailyCode', function ($user) {
             return $user->level == 'creator';
         });
+        Gate::define('lottery.invoicesIndex', function ($user) {
+            return $user->level == 'creator';
+        });
     }
 
     protected function getPermissions()
