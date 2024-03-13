50b02f900368e90041c76100bc405a969b28cc02
hussain1488 <hedayati1488@gmail.com>
2024-03-13 12:51:55 +0330

    some filters added to lottery codes index page.

--- public/back/assets/js/pages/lottery/index.js
+++ public/back/assets/js/pages/lottery/index.js
@@ -1,32 +1,69 @@
 $(document).ready(function () {
     let table = $('#lottery_codes_table');
+    var filter = 'all';
     $('#lottery_codes_table').DataTable({
         searching: true,
         processing: true,
+        orderable: true,
         language: {
             url: window.Laravel.datatable_fa
+        },
+        initComplete: function () {
+            // Add start_date and end_date inputs to the search input row
+            $('.dataTables_filter').append(
+                '<input type="button" data-value="all"  class="btn btn-dark code-filter text-white end_date" value="همه">'
+                // '<div class="col"><input type="text" id="end_date" class="form-control btn btn-dark code-filter text-white" placeholder="فیلتر تا تاریخ..."></div>'
+            );
+            $('.dataTables_filter').append(
+                '<input type="button" data-value="invoice"  class="btn btn-dark code-filter text-white start_date" value="فاکتور خرید">'
+                // '<div class="col"><input type="text" id="start_date" class="form-control btn btn-dark code-filter text-white" placeholder="فیلتر از تاریخ..."></div>'
+            );
+            $('.dataTables_filter').append(
+                '<input type="button" data-value="dailyCode"  class="btn btn-dark code-filter text-white end_date" value="کد روزانه">'
+                // '<div class="col"><input type="text" id="end_date" class="form-control btn btn-dark code-filter text-white" placeholder="فیلتر تا تاریخ..."></div>'
+            );
+            $('.dataTables_filter').append(
+                '<input type="button" data-value="active"  class="btn btn-dark code-filter text-white end_date" value="فعال">'
+                // '<div class="col"><input type="text" id="end_date" class="form-control btn btn-dark code-filter text-white" placeholder="فیلتر تا تاریخ..."></div>'
+            );
+            $('.dataTables_filter').append(
+                '<input type="button" data-value="deactive"  class="btn btn-dark code-filter text-white end_date" value="باطل">'
+                // '<div class="col"><input type="text" id="end_date" class="form-control btn btn-dark code-filter text-white" placeholder="فیلتر تا تاریخ..."></div>'
+            );
+
+            // Add event listener to trigger search on date inputs
+            $('.code-filter').on('click', function () {
+                // Convert Persian dates to Gregorian before sending to server
+                // alert($(this).data('value'));
+
+                filter = $(this).data('value');
+
+                // alert(codes);
+                $('#lottery_codes_table').DataTable().draw();
+            });
         },
         ajax: {
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             },
             url: table.data('action'), // Replace with the actual URL of your data endpoint
-            type: 'get'
+            type: 'GET', // Specify GET method for the Ajax request
+            data: function (d) {
+                d.filter = filter;
+            }
         },
         serverSide: true,
-
+        columnDefs: [
+            {targets: [0, 6], orderable: false} // Disable sorting for the first and last column
+        ],
         columns: [
             {data: 'counter', name: 'counter', title: '#'},
-            {
-                data: 'user',
-                name: 'user',
-                title: 'کاربر'
-            },
+            {data: 'user_id', name: 'user_id', title: 'کاربر'},
             {
                 data: 'code',
                 name: 'code',
                 title: 'کد قرعه کشی',
-                searchable: false
+                searchable: true
             },
             {
                 data: 'source',
@@ -34,63 +71,25 @@ $(document).ready(function () {
                 title: 'منبع کد',
                 render: function (data) {
                     if (data == 'invoice') {
-                        return (
-                            '<span class="badge badge-success">' +
-                            'فاکتور خرید' +
-                            '</span>'
-                        );
+                        return '<span class="badge badge-success">فاکتور خرید</span>';
                     } else {
-                        return (
-                            '<span class="badge badge-info">' +
-                            'کد روزانه' +
-                            '</span>'
-                        );
+                        return '<span class="badge badge-info">کد روزانه</span>';
                     }
                 },
                 searchable: true
             },
             {
-                data: 'weekly_state',
-                name: 'weekly_state',
-                title: 'وضعیت هفتگی',
+                data: 'state',
+                name: 'state',
+                title: 'وضعیت',
                 render: function (data) {
-                    if (data == 0) {
-                        return (
-                            '<span class="badge badge-success">' +
-                            'فعال' +
-                            '</span>'
-                        );
-                    } else {
-                        return (
-                            '<span class="badge badge-danger">' +
-                            'باطل' +
-                            '</span>'
-                        );
-                    }
+                    return data == 'active'
+                        ? '<span class="badge badge-success">فعال</span>'
+                        : '<span class="badge badge-danger">باطل</span>';
                 },
                 searchable: true
             },
-            {
-                data: 'monthly_state',
-                name: 'monthly_state',
-                title: 'وضعیت ماهانه',
-                render: function (data) {
-                    if (data == 0) {
-                        return (
-                            '<span class="badge badge-success">' +
-                            'فعال' +
-                            '</span>'
-                        );
-                    } else {
-                        return (
-                            '<span class="badge badge-danger">' +
-                            'باطل' +
-                            '</span>'
-                        );
-                    }
-                },
-                searchable: true
-            },
+            {data: 'date', name: 'date', title: 'تاریخ', orderable: true},
             {
                 data: 'button',
                 name: 'button',
@@ -99,16 +98,13 @@ $(document).ready(function () {
                     return (
                         '<input type="button" data-id="' +
                         data +
-                        '" value="بیشتر"' +
-                        ' class="lotteryCodeButton btn btn-info btn-sm" />'
+                        '" value="بیشتر" class="lotteryCodeButton btn btn-info btn-sm" />'
                     );
                 }
             }
         ],
         select: 'single',
         drawCallback: function (settings) {
-            // console.log('Draw callback executed');
-            // Reset the counter on each page change
             var api = this.api();
             var rows = api.rows({page: 'current'}).nodes();
             var last = null;
@@ -131,5 +127,61 @@ $(document).ready(function () {
 });
 $(document).on('click', '.lotteryCodeButton', function () {
     let btn = $(this);
-    alert(btn.data('id'));
+    $('.lottery_code_id').val(btn.data('id'));
+    $('#lottery_code_more_modal').modal();
+});
+
+$(document).on('click', '#lottery-winner-code-button', function () {
+    var button = $(this); // Store a reference to the button
+
+    var formData = $('#lottery_winner_form').serialize();
+
+    // Include additional data along with the form data
+    var requestData = formData + '&id=' + $('.lottery_code_id').val();
+    $.ajax({
+        url: $('#lottery_winner_form').attr('action'),
+        type: 'post',
+        data: requestData,
+        success: function (response) {
+            $('#lottery_code_more_modal').modal('hide');
+            if (response.status == 'success') {
+                toastr.success(response.message);
+                $('#lottery_codes_table').DataTable().ajax.reload();
+            } else {
+                toastr.error(response.message);
+            }
+        },
+        beforeSend: function () {
+            block(button); // Use the stored reference to the button
+        },
+        complete: function () {
+            unblock(button); // Use the stored reference to the button
+        }
+    });
+});
+$(document).on('click', '#lottery-code-state-button', function () {
+    var button = $(this); // Store a reference to the button
+    var formData = $('#lotteryStateCodeForm').serialize();
+    // Include additional data along with the form data
+    var requestData = formData + '&id=' + $('.lottery_code_id').val();
+    $.ajax({
+        url: $('#lotteryStateCodeForm').attr('action'),
+        type: 'post',
+        data: requestData,
+        success: function (response) {
+            $('#lottery_code_more_modal').modal('hide');
+            if (response.status == 'success') {
+                toastr.success(response.message);
+                $('#lottery_codes_table').DataTable().ajax.reload();
+            } else {
+                toastr.error(response.message);
+            }
+        },
+        beforeSend: function () {
+            block(button); // Use the stored reference to the button
+        },
+        complete: function () {
+            unblock(button); // Use the stored reference to the button
+        }
+    });
 });
