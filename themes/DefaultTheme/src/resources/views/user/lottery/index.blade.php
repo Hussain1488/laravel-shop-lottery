700ac50069d2a3ec5c53a91da9b7f7ee23ed7488
hussain1488 <hedayati1488@gmail.com>
2024-03-18 12:46:33 +0330

    lottery tabs updated.

--- themes/DefaultTheme/src/resources/views/user/lottery/index.blade.php
+++ themes/DefaultTheme/src/resources/views/user/lottery/index.blade.php
@@ -25,26 +25,51 @@
                             </div>
                         @endif
 
-                        <div class="row mt-1 ml-2 mb-2 mr-1">
-                            <h6>
-                                قرعه کشی!
-                            </h6>
+                        <div class="row mt-1 ml-2 mb-2 mr-1 d-flex justify-content-around">
+                            <div class="col">
+
+                                <h6>
+                                    قرعه کشی!
+                                </h6>
+                            </div>
+                            <div class="col">
+                                <h6>
+                                    <div class="logo-area  mr-2 d-flex justify-content-end">
+                                        <button type="button" href="{{ route('front.index') }}"
+                                            class="border-0 bg-none lottery_code_button">
+                                            <img data-src="{{ theme_asset('img/ghorekeshi.png') }}"
+                                                style="width: 100%;height: auto;
+    "
+                                                alt="{{ option('info_site_title', 'خانه اقساط') }}">
+                                        </button>
+                                    </div>
+                                </h6>
+                            </div>
                         </div>
                         <ul class="nav nav-tabs">
                             <li class="nav-item">
-                                <a class="nav-link active" style="font-size: .625rem" data-toggle="tab" href="#home">
+                                <a class="nav-link {{ request('tab') != 'winner' && request('tab') != 'invoice' ? 'active' : '' }}"
+                                    style="font-size: .625rem" data-toggle="tab" href="#lotteryCodes">
                                     کد های قرعه کشی شما</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" style="font-size: .625rem" data-toggle="tab" href="#menu1">
+                                <a class="nav-link {{ request('tab') == 'invoice' ? 'active' : '' }}"
+                                    style="font-size: .625rem" data-toggle="tab" href="#invoices">
                                     فاکتور های خرید شما
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link {{ request('tab') == 'winner' ? 'active' : '' }}"
+                                    style="font-size: .625rem" data-toggle="tab" href="#winners">
+                                    لیست برنده ها
                                 </a>
                             </li>
 
 
                         </ul>
                         <div class="tab-content ">
-                            <div id="home" class="container tab-pane active my-2 py-3">
+                            <div id="lotteryCodes"
+                                class="container tab-pane {{ request('tab') != 'winner' && request('tab') != 'invoice' ? 'active' : 'fade' }} my-2 py-3">
 
 
                                 <div class="row">
@@ -241,10 +266,11 @@
                                     </div>
                                 @endif
                                 <div class="mt-3">
-                                    {{ $lottery->links('front::components.paginate') }}
+                                    {{ $lottery->appends(['tab' => 'lottery', 'page' => $lottery->currentPage()])->links('front::components.paginate') }}
                                 </div>
                             </div>
-                            <div id="menu1" class="container tab-pane my-2 py-3">
+                            <div id="invoices"
+                                class="container tab-pane my-2 py-3 {{ request('tab') == 'invoice' ? 'active' : 'fade' }}">
 
                                 <div class="row">
                                     <div class="col-md-6 col-12 d-flex justify-content-between align-items-center">
@@ -397,7 +423,164 @@
                                 @endif
 
                                 <div class="mt-3">
-                                    {{ $invoice->links('front::components.paginate') }}
+                                    {{ $invoice->appends(['tab' => 'invoice', 'page' => $invoice->currentPage()])->links('front::components.paginate') }}
+                                </div>
+                            </div>
+                            <div id="winners"
+                                class="container tab-pane my-2 py-3 {{ request('tab') == 'winner' ? 'active' : 'fade' }}">
+
+                                <div class="row">
+                                    <div class="col-md-6 col-12 d-flex justify-content-between align-items-center">
+                                        <div class="form-group d-flex align-items-center">
+                                            <label for="first_name" class="mr-2">
+                                                فاکتور های خرید شما
+                                            </label>
+                                            <div class="d-flex ">
+                                                {{-- <input readonly type="text" class="form-control moneyInput" id="first_name"
+                                                name="first_name" value="{{ $user->wallet->balance ?? 0 }}"
+                                                style="margin-left: 4px"><span> ریال</span> --}}
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                @if ($winners->count() > 0)
+                                    <div class="pc-size" data-screen="pc">
+
+                                        <table class="table table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th>
+                                                        #
+                                                    </th>
+                                                    <th>
+                                                        کاربر
+                                                    </th>
+                                                    <th>
+                                                        کد قرعه کشی
+                                                    </th>
+                                                    <th>
+                                                        نوع قرعه کشی
+                                                    </th>
+                                                    <th>
+                                                        تاریخ قرعه کشی
+                                                    </th>
+                                                </tr>
+                                            </thead>
+                                            @php
+                                                $counter = ($winners->currentPage() - 1) * $winners->perPage() + 1;
+                                            @endphp
+                                            <tbody>
+                                                @foreach ($winners as $key)
+                                                    <tr>
+                                                        <td>
+                                                            {{ $counter++ }}
+                                                        </td>
+                                                        <td>
+                                                            {{ $key->user->first_name . ' ' . $key->user->last_name }}
+                                                        </td>
+                                                        <td>
+                                                            {{ $key->latteryCode->code }}
+                                                        </td>
+                                                        <td>
+                                                            @if ($key->type == 'weekly')
+                                                                <span class="badge badge-primary">هفته ای</span>
+                                                            @elseif($key->type == 'monthly')
+                                                                <span class="badge badge-success">ماهانه</span>
+                                                            @else
+                                                                <span class="badge badge-danger">سالانه</span>
+                                                            @endif
+                                                        </td>
+                                                        <td>
+                                                            {{ jDate($key->lottery_date)->format('Y-m-d') }}
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+                                    <div class="mobile-size " data-screen="mobile">
+                                        {{-- @foreach ($trans as $key)
+                                    <div class=" border rounded mb-1">
+                                        <div class="row pt-1">
+                                            <div class="col mr-2">
+                                                <h6 class="">
+                                                    تاریخ تراکنش:
+
+                                                </h6>
+                                            </div>
+                                            <div class="col">
+                                                <span class="transaction_datetime">
+                                                    {{ jdate($key->created_at)->format('d/M/Y') }}
+                                                    <br>
+                                                    {{ jdate($key->created_at)->format('H:i:s') }}
+                                                </span>
+                                            </div>
+                                        </div>
+
+                                        <div class="row pt-1">
+                                            <div class="col mr-2">
+                                                <h6 class="">
+                                                    نوع تراکنش: </h6>
+                                            </div>
+                                            <div class="col">
+                                                <span class="text-dark">
+                                                    {{ $key->type == 'deposit' ? 'شارژ کیف پول' : 'برداشت ازکیف پول' }}
+                                                </span>
+                                            </div>
+                                        </div>
+                                        <div class="row pt-1">
+                                            <div class="col mr-2">
+                                                <h6 class="">
+                                                    منبع
+                                                </h6>
+                                            </div>
+                                            <div class="col">
+                                                {{ $key->source == 'user' ? 'کاربر' : 'اپراتور' }}
+                                            </div>
+                                        </div>
+                                        <div class="row pt-1">
+                                            <div class="col mr-2">
+
+                                                <h6 class="">
+                                                    مبلغ تراکنش </h6>
+                                            </div>
+                                            <div class="col">
+
+                                                <span
+                                                    class="moneyInputSpan {{ $key->type == 'deposit' ? 'text-success' : 'text-danger' }}">{{ $key->amount }}</span>
+                                                ریال
+                                            </div>
+                                        </div>
+                                        <div class="row pt-1">
+                                            <div class="col mr-2">
+
+                                                <h6 class="">
+                                                    وضعیت </h6>
+                                            </div>
+
+                                            <div class="col">
+                                                <span
+                                                    class="{{ $key->status == 'success' ? 'text-success' : 'text-danger' }}">{{ $key->status == 'success' ? 'موفق' : 'ناموفق' }}</span>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                @endforeach --}}
+                                    </div>
+                                @else
+                                    <div class="p-3">
+                                        <div class="alert alert-warning">
+                                            چیزی برای نمایش نیست!
+                                        </div>
+                                    </div>
+                                @endif
+
+                                <div class="mt-3">
+                                    {{ $winners->appends(['tab' => 'winner', 'page' => $winners->currentPage()])->links('front::components.paginate') }}
                                 </div>
                             </div>
 
@@ -408,7 +591,7 @@
             </div>
         </div>
     </div>
-
+    @include('front::partials.modal')
 
     <!-- End Content -->
 @endsection
