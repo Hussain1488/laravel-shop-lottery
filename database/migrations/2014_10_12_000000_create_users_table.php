d82fc11a084dac145a5d95ea5c87de2288622e49
hussain1488 <hedayati1488@gmail.com>
2024-04-06 17:01:55 +0330

    bithdate, gender and some other fields added for user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,12 +17,16 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('first_name');
             $table->string('last_name');
+            $table->date('birth_date')->nullable();
+            $table->enum('gender', ['male', 'female', 'none'])->default('none');
+            $table->integer('Id_number')->nullable();
             $table->string('username')->unique();
             $table->string('email')->unique()->nullable();
             $table->string('image')->nullable();
             $table->string('level')->default('user');
             $table->string('password');
             $table->index('username');
+            $table->enum('comment_permision', ['valid', 'not_valid'])->default('valid');
             $table->rememberToken();
             $table->timestamps();
         });
