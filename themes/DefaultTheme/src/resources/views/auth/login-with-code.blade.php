9a50ce23ab798469fd568d3af198aa35cf30b8f6
hussain1488 <hedayati1488@gmail.com>
2024-04-16 14:02:27 +0330

    text type changed to nubmer type

--- themes/DefaultTheme/src/resources/views/auth/login-with-code.blade.php
+++ themes/DefaultTheme/src/resources/views/auth/login-with-code.blade.php
@@ -101,11 +101,11 @@
                         <input id="user_phone_number" name="mobile" type="hidden" value="">
                         <div class="email-otp-container d-flex justify-center">
                             <!-- Six input fields for OTP digits -->
-                            <input type="text" class="email-otp-input" pattern="\d" maxlength="1">
-                            <input type="text" class="email-otp-input" pattern="\d" maxlength="1">
-                            <input type="text" class="email-otp-input" pattern="\d" maxlength="1">
-                            <input type="text" class="email-otp-input" pattern="\d" maxlength="1">
-                            <input type="text" class="email-otp-input" pattern="\d" maxlength="1">
+                            <input type="number" class="email-otp-input" pattern="\d" maxlength="1">
+                            <input type="number" class="email-otp-input" pattern="\d" maxlength="1">
+                            <input type="number" class="email-otp-input" pattern="\d" maxlength="1">
+                            <input type="number" class="email-otp-input" pattern="\d" maxlength="1">
+                            <input type="number" class="email-otp-input" pattern="\d" maxlength="1">
 
                         </div>
                         <div class="numbers-verify form-content form-content1">
